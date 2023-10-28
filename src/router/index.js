033a397a5e15f2f7edd2435d6f96505d716631a1
MichealAdeotun <adedotun2096@gmail.com>
2023-10-28 23:24:34 +0100

    made edits in index.js file

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from "vue-router"
+import {createRouter, createWebHashHistory} from "vue-router";
 import HomeView from "../View/HomeView.vue";
 import ContactView from "../View/ContactView.vue";
 import AboutView from "../View/AboutView.vue";
@@ -6,34 +6,18 @@ import WebDesign from "../Services/WebDesign.vue";
 import SocialMedia from "../Services/SocialMedia.vue";
 import BusinessBranding from "../Services/BusinessBranding.vue";
 
+
 const routes = [
-    {
-        path:'/',
-        component: HomeView
-    },
-    {
-        path:'/contact',
-        component: ContactView
-    },
-    {
-        path:'/about',
-        component: AboutView
-    },
-    {
-        path:'/webDesign',
-        component: WebDesign
-    },
-    {
-        path:'/socialMedia',
-        component: SocialMedia
-    },
-    {
-        path:'/businessBranding',
-        component: BusinessBranding
-    }
+    { path: '/', component: HomeView },
+    { path: '/contact', component: ContactView },
+    { path: '/about', component: AboutView },
+    { path: '/webDesign', component: WebDesign },
+    { path: '/socialMedia', component: SocialMedia },
+    { path: '/businessBranding', component: BusinessBranding },
 ]
 
+
 export const router = createRouter({
-    history: createWebHistory(),
-    routes
+    history: createWebHashHistory(),
+    routes,
 })
